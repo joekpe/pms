5633a58f6085664fd9d3506bdafc4e6ef1002666
Joshua Kperator <jkperator@gmail.com>
2018-05-06 20:12:19 +0000

    lecturer dashboard

--- dashboard.php
+++ dashboard.php
@@ -217,6 +217,34 @@
 <?php
     }
 ?>
+
+
+<?php
+    if ($_SESSION['access_level'] == SUPERVISOR) {
+?>
+<div class="">
+
+    <div class="row top_tiles">
+
+        <div class="animated flipInY col-lg-6 col-md-3 col-sm-6 col-xs-12">
+            <div class="tile-stats">
+                <div class="icon"><i class="fa fa-users"></i>
+                </div>
+                <div class="count"><?php echo Pairing::total_assigned_students($_SESSION['user_id']); ?></div>
+
+                <h3>Assigned Students</h3>
+            </div>
+        </div>
+    
+       
+
+    </div>
+</div>
+<?php
+    }
+?>
+
+
                         
 <?php
 	require_once 'layouts/bottom.php';
